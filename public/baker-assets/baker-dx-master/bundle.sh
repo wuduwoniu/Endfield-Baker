b6201859e6abbf3c3f033fd0e3f21155f97b75dd
user <user@local.dev>
2026-05-05 15:51:33 +0800

    chore: remove baker-dx-master from repo, add to .gitignore

--- public/baker-assets/baker-dx-master/bundle.sh
+++
@@ -1,5 +0,0 @@
-rustup target add wasm32-unknown-unknown
-
-cargo install dioxus-cli
-
-dx bundle --release --platform web
--- public/baker-assets/baker-dx-master/src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod baker;
-
-pub mod assets;
-
-#[cfg(test)]
-mod tests;
